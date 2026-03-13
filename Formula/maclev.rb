c252e4226f2b2752557e65370bf078a188c6eade
SHi-ON <shawn@azdam.com>
2026-03-13 15:48:20 -0400

    fix: disable SwiftPM sandbox in Homebrew formula

--- Formula/maclev.rb
+++ Formula/maclev.rb
@@ -1,16 +1,20 @@
 class Maclev < Formula
   desc "Any-window floating utility for macOS"
   homepage "https://github.com/Aureuma/maclev"
-  url "https://github.com/Aureuma/maclev.git",
-      tag: "v0.1.0",
-      revision: "1796af1ec810fea6222a4bc8a294d33ffc3390cf"
+  url "https://github.com/Aureuma/maclev/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "76785338eda72f6697cb71414e07408739672d13f3333d3f6ebca831572bb0ef"
   license "MIT"
   head "https://github.com/Aureuma/maclev.git", branch: "main"
 
   depends_on :macos
 
   def install
-    system "swift", "build", "-c", "release"
+    ENV["SWIFTPM_DISABLE_SANDBOX"] = "1"
+    system "swift", "build", "--disable-sandbox", "-c", "release"
     bin.install ".build/release/maclev"
+  end
+
+  test do
+    assert_predicate bin/"maclev", :exist?
   end
 end
