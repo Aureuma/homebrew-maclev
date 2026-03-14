fbb3ce28376cbd1840b87480d4a483f6c1da10aa
SHi-ON <shawn@azdam.com>
2026-03-14 18:34:43 -0400

    Fix maclev 0.4.16 cask checksum

--- Casks/maclev.rb
+++ Casks/maclev.rb
@@ -1,6 +1,6 @@
 cask "maclev" do
   version "0.4.16"
-  sha256 "d0a34fa9f31486de98cd2bdfd0a354459602324596d9efccf9f8ce8604dd4731"
+  sha256 "251a6fb05507a43465042d5a4fc1ec9beb385ad0b01c61cc96848fe4108c37ce"
 
   url "https://raw.githubusercontent.com/Aureuma/homebrew-maclev/main/artifacts/maclev-#{version}.zip"
   name "MacLev"
