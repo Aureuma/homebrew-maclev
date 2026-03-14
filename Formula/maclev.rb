2bdbe1c7dee3dce083f7df9dd9d6de45c81fa757
SHi-ON <shawn@azdam.com>
2026-03-14 10:47:31 -0400

    chore: update maclev formula to v0.2.1

--- Formula/maclev.rb
+++ Formula/maclev.rb
@@ -1,8 +1,8 @@
 class Maclev < Formula
   desc "Lightweight floating browser for macOS"
   homepage "https://github.com/Aureuma/maclev"
-  url "https://github.com/Aureuma/maclev/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "b8bd8ce310838f75e8e36252bb4a1c7ce3c2ca3844b5a60ef4826507897a6484"
+  url "https://github.com/Aureuma/maclev/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "8e404cfca330548eeface334a8c762634a23f186b8bcdbe0871975bb812048e3"
   license "MIT"
   head "https://github.com/Aureuma/maclev.git", branch: "main"
 
