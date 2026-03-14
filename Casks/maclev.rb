dd71840a965e0cab51cd272df1d71bf6d856e8a0
SHi-ON <shawn@azdam.com>
2026-03-14 12:26:14 -0400

    chore: update maclev cask to v0.4.3

--- Casks/maclev.rb
+++ Casks/maclev.rb
@@ -1,6 +1,6 @@
 cask "maclev" do
-  version "0.4.2"
-  sha256 "d66391bc6d625ebbe80230d69664470fc0722fd6432f79bb6b38a1a783e67a88"
+  version "0.4.3"
+  sha256 "ed56837bf1837380ec2b4340fa0808f8fb6bae40ceca66a7d3e1d294427cbb38"
 
   url "https://raw.githubusercontent.com/Aureuma/homebrew-maclev/main/artifacts/maclev-#{version}.zip"
   name "maclev"
