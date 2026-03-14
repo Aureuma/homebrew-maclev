61bd5a63b1f6c2173450ec21a7eb0f1392c658a0
SHi-ON <shawn@azdam.com>
2026-03-14 16:15:37 -0400

    Update cask to maclev 0.4.8

--- Casks/maclev.rb
+++ Casks/maclev.rb
@@ -1,9 +1,9 @@
 cask "maclev" do
-  version "0.4.7"
-  sha256 "686b19e367426445b57078fc2d46348c937a8d630bdc78fa4f7c9bac7c6a34cd"
+  version "0.4.8"
+  sha256 "5835e7607544edbe53a1621f1ea72dd5bfa25b4a2047cdc8777af2ab2ce88c35"
 
   url "https://raw.githubusercontent.com/Aureuma/homebrew-maclev/main/artifacts/maclev-#{version}.zip"
-  name "maclev"
+  name "MacLev"
   desc "Lightweight floating browser for macOS"
   homepage "https://github.com/Aureuma/maclev"
 
